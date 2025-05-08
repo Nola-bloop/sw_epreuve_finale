013f35e765f2b5a135a1c94db9318e2be1fc372c
Victo-2343383 <2343383@carrefour.cegepvicto.ca>
2025-05-08 09:06:25 -0400

    postgre correction

--- src/config/db.js
+++ src/config/db.js
@@ -5,11 +5,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 const db = new Pool({
-    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: 5432,
+    max: 10
 });
 
 export default db;
